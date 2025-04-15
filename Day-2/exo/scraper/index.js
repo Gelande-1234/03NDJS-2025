82634084c6faefca8c93f5a7f1f8688ad864fcc9
Gelande-1234 <angegelande@gmail.com>
2025-04-15 17:09:55 +0200

    recuperation des données dans un fichier json

--- Day-2/exo/scraper/index.js
+++ Day-2/exo/scraper/index.js
@@ -1,13 +1,12 @@
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 async function scraper() {
   try {
-   
     const $ = await cheerio.fromURL('https://www.geograf.in/fr/table.php');
 
     const tableau = [];
 
-    
     $('table.nice.full.responsive tbody tr').each((i, el) => {
       const tds = $(el).find('td');
       tableau.push({
@@ -20,11 +19,18 @@ async function scraper() {
       });
     });
 
-    console.log(tableau);
+    
+    const jsonData = JSON.stringify(tableau, null, 2);
+
+    
+    fs.writeFileSync('donnees_pays.json', jsonData, 'utf8');
+
+    console.log('✅ Données exportées dans donnees_pays.json');
   } catch (err) {
     console.error('Erreur :', err.message);
   }
 }
 
 scraper();
+
 
