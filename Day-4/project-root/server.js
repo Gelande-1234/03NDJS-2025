1da8cf1f1b89678e3d5f576b5fc31533507af81b
Gelande-1234 <angegelande@gmail.com>
2025-04-17 13:21:22 +0200

    configuration du serveur

---
+++ Day-4/project-root/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/", authRoutes);
+app.use("/", userRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
