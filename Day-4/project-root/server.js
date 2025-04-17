5c2fc7ec5ce7ac97b92d6d69fcb0f56510756847
Gelande-1234 <angegelande@gmail.com>
2025-04-17 17:04:23 +0200

    test de demarrage du serveur

--- Day-4/project-root/server.js
+++ Day-4/project-root/server.js
@@ -5,8 +5,13 @@ import userRoutes from "./routes/user.js";
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.use("/", authRoutes);
 app.use("/", userRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
