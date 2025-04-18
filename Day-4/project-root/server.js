71428bb7f5ee1333bfe93b40f289e09dbf5a5406
Gelande-1234 <angegelande@gmail.com>
2025-04-18 11:42:34 +0200

    creation du fichier de lancement de mongodb

--- Day-4/project-root/server.js
+++ Day-4/project-root/server.js
@@ -1,34 +1,30 @@
+
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
+import connectDB from "./db.js"; 
 
-
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-// Route de test
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-// Routes pour l'authentification et les utilisateurs
-app.use("/", authRoutes);
-app.use("/", userRoutes);
 
-// Connexion à MongoDB
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/monapp";  // Utilise la variable d'environnement MONGO_URI
-const PORT = process.env.PORT || 3000;  // Utilise la variable d'environnement
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connexion à MongoDB réussie");
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-  })
-  .catch((err) => {
-    console.error("Erreur MongoDB :", err.message);
-  });
+
+const PORT = process.env.PORT || 3000;
+
+connectDB(); 
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 
